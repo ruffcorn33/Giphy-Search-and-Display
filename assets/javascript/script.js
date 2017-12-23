6139793f18d90b8f821c8381262fee066aef412b
Craig Christensen <ruffcorn@gmail.com>
2017-12-23 10:32:10 -0600

    Fully funtional

---
+++ assets/javascript/script.js
@@ -0,0 +1,58 @@
+
+$(".submit").click(function(event) {
+  event.preventDefault();
+  var animal = $("#searchTerm").val();
+  var animalBtn = $("<button class='animal-btn m-1'>");
+  animalBtn.attr('data-animal', animal);
+  animalBtn.text(animal);
+  $("#btns-appear-here").append(animalBtn);
+  $("#searchTerm").val("");
+});
+
+// using a delegated event handler because the dynamically added buttons
+// don't have the click event bound to them.
+$('#btns-appear-here').on('click', '.animal-btn', function(){
+  //clear any previous gifs
+  $('.item').remove();
+  //build queryURL
+  var URLprefix = "https://api.giphy.com/v1/gifs/search?q=";
+  var animal = $(this).attr("data-animal");
+  var URLpostfix =  "&api_key=ibnIqcKEQABVaS4GlYkw32oe06vsK7Sg&limit=20";
+  var queryURL = URLprefix+animal+URLpostfix;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div class='item float-left m-2'>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var animalImage = $("<img>");
+      animalImage.attr("src", results[i].images.fixed_height_still.url);
+      animalImage.attr("data-animate", results[i].images.fixed_height.url);
+      animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+      animalImage.attr("data-state", "still");
+      animalImage.attr("class", "gif");
+      gifDiv.prepend(p);
+      gifDiv.prepend(animalImage);
+
+      $("#gifs-appear-here").prepend(gifDiv);
+    }
+  });
+});
+
+//toggle the gif animation
+//using another delegated event handler for teh dynamically added images
+$('#gifs-appear-here').on('click', '.gif', function(){
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
